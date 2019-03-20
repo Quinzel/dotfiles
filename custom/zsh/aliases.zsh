44d6cb28039931dea6be05d58b28a91e62f1048e
ppodczasi <ppodczasi@nvidia.com>
2019-03-20 13:38:00 +0100

    fixed cdls

--- custom/zsh/aliases.zsh
+++ custom/zsh/aliases.zsh
@@ -3,13 +3,13 @@ if [ -x "$(command -v lsd)" ]; then
   alias ls='lsd'
 fi
 
-cd_ls() {
-  cd $1; 
-  ls -a
-}
-
-alias cd='cd_ls'
 alias ll='ls -lh'
-alias la='ls -ah'
+alias la='ls -a'
 alias lla='ls -lah'
 alias lt='ls --tree'
+
+cdls() {
+  builtin cd "$@" && la
+}
+  
+alias cd='cdls'
